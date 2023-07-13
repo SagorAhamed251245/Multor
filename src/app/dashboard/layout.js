afdfe0a95ffe4ebcd603c62b4d1e63d283468e60
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-07-13 17:19:54 +0600

    layout

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,11 @@
+
+const DashboardLayout = ({children}) => {
+    return (
+        <div>
+            <h1>Side bar</h1>
+            {children}
+        </div>
+    );
+};
+
+export default DashboardLayout;
