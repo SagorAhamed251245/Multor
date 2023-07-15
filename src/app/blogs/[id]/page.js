92214bd0a673104fbb34eb82042fb53ddd05d2fb
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-07-15 22:22:39 +0600

    some cnage

---
+++ src/app/blogs/[id]/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SingleBlogs = ({params}) => {
+    console.log(params);
+    return (
+        <div>
+            <h1>Single</h1>
+        </div>
+    );
+};
+
+export default SingleBlogs;
---
+++ src/app/blogs/page.js
@@ -0,0 +1,11 @@
+
+const BlogsPage = () => {
+   ;
+    return (
+        <div>
+            <h1>blogs</h1>
+        </div>
+    );
+};
+
+export default BlogsPage;
