855660f234c37e4f85b3d471f1199c3d81dc4379
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-07-16 16:32:11 +0600

    page not found done

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='text-center bg-green-400 w-screen h-screen'>
+            <h2>Page Not Found</h2>
+        </div>
+    );
+};
+
+export default NotFound;
