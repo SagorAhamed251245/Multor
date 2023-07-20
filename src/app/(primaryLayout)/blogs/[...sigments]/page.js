b0b22d821db4374dcf1357c517bc054b6776ae05
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-07-20 09:06:43 +0600

    serch prames

--- src/app/(primaryLayout)/blogs/[...sigments]/page.js
+++ src/app/(primaryLayout)/blogs/[...sigments]/page.js
@@ -1,10 +1,21 @@
-import React from 'react';
+"use client"
 
-const SingleBlogs = ({params}) => {
-    console.log(params);
+import { useParams, useSearchParams } from "next/navigation";
+
+
+
+const SingleBlogs = ({params, searchParams}) => {
+  const params2 = useParams();
+  console.log("🚀 ~ file: page.js:9 ~ SingleBlogs ~ params2:", params2.segments)
+
+  const searchParams2 = useSearchParams()
+  console.log("🚀 ~ file: page.js:11 ~ SingleBlogs ~ searchParams2:", searchParams2.get('title'))
+
+
+  
     return (
         <div>
-            <h1>Single</h1>
+            <h1>{searchParams.title}</h1>
         </div>
     );
 };
