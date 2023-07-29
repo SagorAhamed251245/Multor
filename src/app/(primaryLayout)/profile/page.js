5c9f7090b997a3868417bc78b4474ae331ac4e3d
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-07-29 17:03:46 +0600

    image optimaiz

--- src/app/(primaryLayout)/profile/page.js
+++ src/app/(primaryLayout)/profile/page.js
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import React from 'react';
+import image1 from '../../../assets/part-blurry-image.png'
 
 const ProfilePage = () => {
     return (
         <div>
             <h1>This is profile</h1>
+            <Image width={1200} src={image1} alt='' placeholder=''></Image>
+           
         </div>
     );
 };
