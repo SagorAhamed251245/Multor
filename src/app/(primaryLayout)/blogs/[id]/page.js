7633c1f511f78fb39a348ef67ba04a140ba75072
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-07-30 22:17:17 +0600

    metadata

--- src/app/(primaryLayout)/blogs/[id]/page.js
+++ src/app/(primaryLayout)/blogs/[id]/page.js
@@ -1,14 +1,21 @@
 import loadSingleBlog from "@/utils/loadSingleBlog";
 
+export const generateMetadata = async ({ params }) => {
+  const { title } = await loadSingleBlog(params.id);
+  return {
+    title: title,
+  };
+};
 const SingleBlogs = async ({ params }) => {
-  const { id, body, title } = await loadSingleBlog(params.id);
+  const { title, id, body } = await loadSingleBlog(params.id);
   return (
     <div>
       <div className="border-sky-400 border w-10/12 mx-auto mt-28">
-        <p> {id}.{title}</p>
         <p>
-          {body}
+          {" "}
+          {id}.{title}
         </p>
+        <p>{body}</p>
       </div>
     </div>
   );
--- src/utils/loadSingleBlog.js
+++ src/utils/loadSingleBlog.js
@@ -1,9 +1,8 @@
-
 const loadSingleBlog = async (id) => {
-   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-      cache: "no-cache"
-    })
-   return res.json()
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-cache",
+  });
+  return res.json();
 };
 
 export default loadSingleBlog;
