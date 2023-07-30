3d4b0cee0079cf3df7c83996a114bee00b2a4312
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-07-30 22:01:22 +0600

    daynamic data

--- src/app/(primaryLayout)/blogs/[id]/page.js
+++ src/app/(primaryLayout)/blogs/[id]/page.js
@@ -1,7 +1,17 @@
-const SingleBlogs = ({ params }) => {
-  return <div>
-    <h1>{params.id}</h1>
-  </div>;
+import loadSingleBlog from "@/utils/loadSingleBlog";
+
+const SingleBlogs = async ({ params }) => {
+  const { id, body, title } = await loadSingleBlog(params.id);
+  return (
+    <div>
+      <div className="border-sky-400 border w-10/12 mx-auto mt-28">
+        <p> {id}.{title}</p>
+        <p>
+          {body}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SingleBlogs;
---
+++ src/utils/loadSingleBlog.js
@@ -0,0 +1,9 @@
+
+const loadSingleBlog = async (id) => {
+   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      cache: "no-cache"
+    })
+   return res.json()
+};
+
+export default loadSingleBlog;
